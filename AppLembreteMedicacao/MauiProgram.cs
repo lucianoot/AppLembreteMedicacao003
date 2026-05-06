4448b55cd2e844c76e15bebee1edfa09ba54b3a2
VeronicaYukie <veronica.ypires@gmail.com>
2026-05-06 00:23:04 -0300

    Build estável: Erros de compilação corrigidos, mas lógica de botões em progresso.

--- AppLembreteMedicacao/MauiProgram.cs
+++ AppLembreteMedicacao/MauiProgram.cs
@@ -7,56 +7,10 @@ namespace AppLembreteMedicacao
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
-        {
-            // 1. Configuração de Cultura
-            var culturaBr = new CultureInfo("pt-BR");
-            CultureInfo.DefaultThreadCurrentCulture = culturaBr;
-            CultureInfo.DefaultThreadCurrentUICulture = culturaBr;
-
+        { 
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                // 2. Configuração das Notificações e Botões
-                .UseLocalNotification(config =>
-                {
-                    config.AddCategory(new NotificationCategory(NotificationCategoryType.Status)
-                    {
-                        ActionList = new HashSet<NotificationAction>
-                        {
-                            new NotificationAction(100) { Title = "Dose Tomada" },
-                            new NotificationAction(101) { Title = "Pular Dose" }
-                        }
-                    });
-                })
-                // 3. Configuração de Fontes
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
-#if DEBUG
-            builder.Logging.AddDebug();
-#endif
-
-            return builder.Build();
-        } 
-    
-    }
-}
-
-       /* public static MauiApp CreateMauiApp()
-        {
-            // Defina a cultura para Português do Brasil
-            var culturaBr = new CultureInfo("pt-BR");
-            CultureInfo.DefaultThreadCurrentCulture = culturaBr;
-            CultureInfo.DefaultThreadCurrentUICulture = culturaBr;
-
-            var builder = MauiApp.CreateBuilder(); builder
-            .UseMauiApp<App>()
-                var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
                 .UseLocalNotification(config =>
                 {
                     config.AddCategory(new NotificationCategory(NotificationCategoryType.Status)
@@ -73,19 +27,56 @@ namespace AppLembreteMedicacao
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-            .ConfigureFonts(fonts =>
-     {
-
-         fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-         fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-
-     });
 
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
-}*/
+}
+
+
+/*
+// Defina a cultura para Português do Brasil
+var culturaBr = new CultureInfo("pt-BR");
+             CultureInfo.DefaultThreadCurrentCulture = culturaBr;
+             CultureInfo.DefaultThreadCurrentUICulture = culturaBr;
+
+             var builder = MauiApp.CreateBuilder(); builder
+             .UseMauiApp<App>()
+                 var builder = MauiApp.CreateBuilder();
+             builder
+                 .UseMauiApp<App>()
+                 .UseLocalNotification(config =>
+                 {
+                     config.AddCategory(new NotificationCategory(NotificationCategoryType.Status)
+                     {
+                         ActionList = new HashSet<NotificationAction>
+                         {
+                             new NotificationAction(100) { Title = "Dose Tomada" },
+                             new NotificationAction(101) { Title = "Pular Dose" }
+                         }
+                     });
+                 })
+                 .ConfigureFonts(fonts =>
+                 {
+                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                 });
+             .ConfigureFonts(fonts =>
+        {
+
+          fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+          fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+        });
+
+        #if DEBUG
+             builder.Logging.AddDebug();
+        #endif
+
+             return builder.Build();
+         }
+        }
+        }*/
